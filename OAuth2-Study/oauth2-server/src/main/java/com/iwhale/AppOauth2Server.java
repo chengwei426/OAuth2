669936d893cd8a6de896fca059a9d6295d000490
chengwei426 <chengwei426@126.com>
2019-12-29 15:40:17 +0800

    ooauth2.0

---
+++ OAuth2-Study/oauth2-server/src/main/java/com/iwhale/AppOauth2Server.java
@@ -0,0 +1,26 @@
+package com.iwhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * 1. 生成授权连接
+ * http://localhost:8080/oauth/authorize?response_type=code&client_id=clientId_1&redirect_uri=https://www.baidu.com/
+ *
+ * 2. 使用授权码获取accessToken
+ *   http://localhost:8080/oauth/token?grant_type=authorization_code&code=Wmw9Xj&redirect_uri=https://www.baidu.com/&scope=all
+ *
+ *   3. 验证token
+ *   http://localhost:8080/oauth/check_token?token=4333481d-39c3-4c58-96c2-fc74bbbf6fe4
+ *
+ */
+@SpringBootApplication
+public class AppOauth2Server
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AppOauth2Server.class, args);
+    }
+}
